34361b059d77eb97f5b983fe73a8e5431ebd4634
ateszki <ateszki@gmail.com>
2018-01-22 17:30:33 -0300

    ajustes db

--- src/migrations/2017_11_06_193152_update_localidades_geo.php
+++ src/migrations/2017_11_06_193152_update_localidades_geo.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateLocalidades extends Migration
+class UpdateLocalidadesGeo extends Migration
 {
     /**
      * Run the migrations.
